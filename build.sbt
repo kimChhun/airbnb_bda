c66835bcd3fb928fa2aec7a0bc441c8b25837421
kimChhun <kimtaing@MacBook-Pro-de-kim.local>
2017-05-28 15:34:16 +0200

    added spkark-sql dependencies

--- build.sbt
+++ build.sbt
@@ -4,6 +4,7 @@ version := "0.0.1"
 
 scalaVersion := "2.11.8"
 
+val sparkVersion = "2.1.0"
 
 // grading libraries
 libraryDependencies += "junit" % "junit" % "4.10" % "test"
@@ -11,5 +12,9 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "1.5.1" % "provided"
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion,
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+  "org.apache.spark" %% "spark-hive" % sparkVersion
 )
--- src/main/scala/airbnb/Main.scala
+++ src/main/scala/airbnb/Main.scala
@@ -6,15 +6,41 @@ import org.apache.spark.SparkContext._
 
 import org.apache.spark.rdd.RDD
 import scala.util.matching.Regex
+import org.apache.spark
 
+import java.util.Date
+import org.apache.spark.sql.types
+import org.apache.spark.sql._
 object Main {
   
+  //1: Create spark session
+  val spark: SparkSession =
+    SparkSession
+      .builder()
+      .appName("Spark SQL Test")
+      .config("spark.master", "local")
+      .getOrCreate()
+      
+  // always import implicits so that Spark can infer types when creating Datasets
+  import spark.implicits._
+
+  /*class User(id:String, date_account_created:Date, timestamp_first_active: String
+      , date_first_booking: String, gender: Double, age:Double
+      , signup_method:Double, signup_flow:String, language:String
+      , affiliate_channel:Double,affiliate_provider:Double,first_affiliate_tracked:String,signup_app:String
+      ,first_device_type:String,first_browser:String, country_destination:String)
+*/
+  												
   val conf: SparkConf = new SparkConf().setMaster("local[4]").setAppName("airbnb")
   val sc: SparkContext = new SparkContext(conf)
   
   def sum(rdd: RDD[Int]): Int = rdd.reduce(_+_)
   
   def main(args: Array[String]) {
-    print("1 + 2 + ... + 10000 = " + sum(sc.parallelize(1.to(10000))) + " :)")
+    //print("1 + 2 + ... + 10000 = " + sum(sc.parallelize(1.to(10000))) + " :)")
+    
+    val df = spark.read.option("header", true).
+      option("inferSchema", true).
+      csv("/Users/kimtaing/Documents/github/P_bigData/data/train_users_2.csv")
   }
 }
